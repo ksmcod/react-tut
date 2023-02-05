a24cbea9af845c39117ef50e01266f6f80f2a896
Kono Steve <stevekono77@gmail.com>
2023-02-05 17:54:41 +0100

    Completed the nav with the airbnb logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/airbnb1.png'
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src={logo} alt="Airbnb Logo" srcset="" />
+        </nav>
+    )
+}
